6ac8304b8693f75bd138071b100354da5347683f
daisukesone <sonedai0213@gmail.com>
2018-02-09 16:05:42 +0900

    initial commit

--- Techpod/ViewController.swift
+++ Techpod/ViewController.swift
@@ -8,16 +8,34 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController , UITableViewDataSource{
+    
+    //storyboardで使うtableviewを宣言
+    @IBOutlet var table: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+        
+        //テーブルビューのデータソースメソッドはviewcontrollerクラスに書くよ、という設定
+        table.dataSource = self    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    //セルの数を設定
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+         return 10
+    }
+    
+    //ID付きのセルを取得して、セル付属のtextLabelに「テスト」と表示してみる
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")
+        
+        cell?.textLabel?.text = "テスト"
+        
+        return cell!
     }
 
 
